c6738b5937907f5e879e93b192dca306b19dbcbd
irynadosiak <43314418+irynadosiak@users.noreply.github.com>
2020-12-16 11:29:11 +0200

    Update graphics.py

--- lab1/graphics.py
+++ lab1/graphics.py
@@ -1,82 +1,69 @@
 import matplotlib.pyplot as plt
-import pylab
-def show(database, column,c):
-    # different graphics 
-    a=['greenyellow', 'olive', 'yellow', 'red', 'green', 'plum', 'yellow']
-    x = list(database.index.values)
-    y = list(database[column])
-    plt.scatter(x,y, label=column, color=a[c])
-    plt.xticks(rotation=90)
-    plt.ylabel(column)
-    plt.legend()
-    plt.grid()
-    plt.show()
-        
-def show_one(database,column,c):
-    if column == 'Wind Speed':
-        x = list(database.index.values)
-        y = list(database[column])
-        database[column].plot.bar(color = 'gold')
-        plt.ylabel(column)
-        plt.xticks(rotation=90)
-        plt.legend()
-        plt.grid()
-        plt.show()   
-    elif column == 'Humidity':
-        x = list(database.index.values)
-        y = list(database[column])
-        database[column].plot.area(color='palevioletred')
-        plt.ylabel(column)
-        plt.xticks(rotation=90)
-        plt.legend()
-        plt.show() 
-    elif column == 'Wind' or column == 'Condition':
-        a=['greenyellow', 'olive', 'yellow', 'red', 'green', 'plum', 'yellow']
-        x = list(database.index.values)
-        y = list(database[column])
-        plt.scatter(x,y, label=column, color=a[c])
-        plt.xticks(rotation=90)
-        plt.ylabel(column)
-        plt.grid()
-        plt.legend()
-        plt.show()
-    else:
-        x = list(database.index.values)
-        y = list(database[column])
-        x1 = range(len(x))
-        pylab.xticks(x1, x)
-        plt.xticks(x1,rotation=90)
-        plt.plot(x1, y, label=column)
-        plt.scatter(x,y)
-        plt.xticks(rotation=90)
-        plt.ylabel(column)
-        plt.legend()
-        plt.show()
+import numpy as np
+from sys import exit
 
-def create(database, columns, how_show):
-    # create graphics 
-    x=1
-    c=0
-    if len(columns) == 1:
-        for column in columns:
-            how_show(database, column, c)
-            c=c+1 
-    elif len(columns) == 2:
-        if 'Wind' in columns or 'Condition' in columns:
-            for column in columns:
-                plt.subplot(len(columns), 1, x)
-                how_show(database, column, c)
-                x=x+1
-                c=c+1
-        else:
-            for column in columns:
-                how_show(database, column, c)
-                c=c+1 
+def is_numeric(df, data):
+    if df[data].dtype == np.int64 or df[data].dtype == np.float:
+        return True
     else:
-        for column in columns:
-            plt.subplot(len(columns), 1, x)
-            how_show(database, column, c)
-            x=x+1
-            c=c+1
-        
-        
+        return False
+
+def validation(df, data, kind):
+    for i in range(0,len(data)):
+        if kind == 3 or kind == 1 and (not is_numeric(df, data[i])):
+            valid = False
+        else:
+            valid = True
+    return valid
+
+def plotting(df, data, index, kind):   
+    check = validation(df, data, kind)
+    while not check:
+        print('This kind of plot can\'t use not scalar value!')
+        exit()
+    if data == None:
+        data = ['Temperature','Dew Point', 'Humidity', 'Wind Speed', 'Wind Gust', 'Pressure', 'Precip.', 'Precip Accum']
+    if index != 'Time' and kind == 1:
+        df = df.groupby(df['day/month']).mean().reset_index()   
+    if index in data:
+        print('We can\'t plot a chart with the same values of x and y!')
+        print('We will exchange x to default day/month!')        
+        df.set_index('day/month', inplace=True)   
+    else:
+        df.set_index(index, inplace=True)  
+    if kind == 1:
+        x = list(df.index.values)
+        for col in data:
+            if 'Wind' in data or 'Condition' in data:
+                plt.figure()
+            y = list(df[col])
+            plt.plot(x,y, label=col)
+            plt.xticks(rotation=90)
+            plt.xlabel(index) 
+            plt.legend()
+            plt.title('Line chart')
+        plt.show()
+    elif kind == 2:
+        x = list(df.index.values)
+        for col in data:
+            if 'Wind' in data or 'Condition' in data:
+                plt.figure()
+            y = list(df[col])
+            plt.scatter(x,y, label=col)
+            plt.xticks(rotation=90)
+            plt.xlabel(index) 
+            plt.legend()
+            plt.title('Scatter chart')
+        plt.show()
+    elif kind == 3:
+        for col in data:
+            df[col].plot.hist(alpha=0.5)
+            plt.legend()
+            plt.title('Histogram')
+            plt.show()  
+    elif kind == 4:
+        for col in data:
+            plt.figure()
+            df.groupby(col)[col].count().plot.pie(autopct='%1.1f%%')
+            plt.title('Pie Chart')
+
