5e1a1bb71121a499e42c4971fc2682c294524502
irynadosiak <43314418+irynadosiak@users.noreply.github.com>
2020-12-16 11:29:42 +0200

    Update lab1.py

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -1,11 +1,11 @@
-import pandas as pd
+import pandas
 import graphics
 from datetime import datetime
 
 def to_celsius(temperature1):
     b=[]
     for i in range(len(temperature1)):
-        celsius = int(temperature1[i] - 32) * 5/9
+        celsius = int((temperature1[i] - 32) * 5/9)
         b.append(celsius)
     return b
 
@@ -24,50 +24,79 @@ def to_int(list1, x):
         list2.append(int(str(list1[i])[:x]))
     return list2
 
-def to_float(number1):
-    # change pressure to float     
-    k=[]
-    for i in range(len(number1)):
-        b=number1[i].split(',')
-        c='.'.join(map(str, b))
-        d=float(c)
-        k.append(d)
-    return k
-
 def parse(database):
     # parse some databases 
-    database['Pressure'] = database['Pressure'].replace(to_replace=list(database['Pressure']),value=to_float(list(database['Pressure'])))
-    database['Time'] = pd.to_datetime(database['Time']).apply(lambda x: x.strftime(r'%H:%M'))
-    database['Humidity'] = database['Humidity'].replace(to_replace=list(database['Humidity']),value=to_int(list(database['Humidity']), -1))
-    database['Wind Speed'] = database['Wind Speed'].replace(to_replace=list(database['Wind Speed']),value=to_int(list(database['Wind Speed']),-4))
-    database['Wind Gust'] = database['Wind Gust'].replace(to_replace=list(database['Wind Gust']),value=to_int(list(database['Wind Gust']),-4))
-    database['day/month'] = database['day/month'].replace(to_replace=list(database['day/month']),value=change_date(list(database['day/month'])))
-    database['Temperature'] = database['Temperature'].replace(to_replace=list(database['Temperature']),value=to_celsius(list(database['Temperature'])))
+    database['Time'] = pandas.to_datetime(database['Time']).apply(lambda x: x.strftime(r'%H:%M'))
+    database['Humidity'] = to_int(list(database['Humidity']), -1)
+    database['Wind Speed'] = to_int(list(database['Wind Speed']),-4)
+    database['Wind Gust'] = to_int(list(database['Wind Gust']),-4)
+    database['day/month'] = change_date(list(database['day/month']))
+    database['Temperature'] = to_celsius(list(database['Temperature']))
     return database  
                      
 # read from csv
-database=pd.read_csv('DATABASE.csv', sep=';')  
+database=pandas.read_csv('DATABASE.csv', sep=';',decimal=',')  
 # parse database 
 parse(database)
 # create database depending on your choice of day
 def database_day(database, str):
     database1=database.loc[database['day/month'] == str]
-    database1.set_index('Time', inplace=True)
-    return database1   
+    return database1
 # user interaction
-print('How many charts do you want to vizualizate?') 
-n=int(input())
-print('\nWhich column do you want to vizualizate?') 
-columns=[]
-for j in range(n):
-    column=input() 
-    columns.append(column)
-print('\nDo you want to watch all period or a certain day?(all or dd.mm.yyyy)')
-i = input()
-# create graphics 
-if i=='all':
-    # set index     
-    database.set_index('day/month', inplace=True)
-    graphics.create(database, columns, graphics.show)
-else:
-    graphics.create(database_day(database, i), columns, graphics.show_one)  
+print('Do you want to specify column names to visualize? (Yes or No)')
+while True:
+    choose = input()
+    if choose == 'Yes':
+        print('How many charts do you want to visualize?')
+        while True:
+            n = input()
+            try:
+                if int(n) <= 0:  
+                    print("Sorry, input must be a positive integer, try again")
+                    continue
+                break
+            except:
+                print('There must be a positive number!')  
+        data=[]
+        for i in range(int(n)):
+            column = str(input())
+            data.append(column)
+    elif choose == 'No': 
+        data = None
+    else:
+        print('Yes or No')
+        continue
+    break
+print('Do you want to specify index? (Yes or No)')
+while True:
+    choose = input()
+    if choose == 'Yes':
+        print('Give index:')
+        index = str(input())
+        if index == 'Time':
+            print('Set a specific day (16.07.2019)')
+            day = str(input())
+            database = database_day(database,day)
+    elif choose == 'No': 
+        index = 'day/month'
+    else:
+        print('Yes or No')
+        continue
+    break
+print('Do you want to specify kind of chart? (Yes or No)')
+while True:
+    choose = input()
+    if choose == 'Yes':
+        print('1 - line chart')
+        print('2 - scatter chart')
+        print('3 - histogram')        
+        print('4 - pie chart')        
+        print('Give index:')
+        kind = int(input())
+    elif choose == 'No': 
+        kind = 1
+    else:
+        print('Yes or No')
+        continue
+    break
+graphics.plotting(database, data, index, kind)
